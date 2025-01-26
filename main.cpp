cc0a64dc2d718f2073e59ace84efa18dae8355a5
Jenna Rivera <j3nna.riv@gmail.com>
2025-01-26 11:04:42 -0800

    updateed

--- main.cpp
+++ main.cpp
@@ -7,34 +7,16 @@
 ** Output: both player's outcomes, total points, and this winner
 *********************************************************************/
 
-// In some assignments, you will be provided with a bit of "skeleton code"
-// to get you started. However, this assignment is largely geared toward
-// getting you up to speed on the basics of C++ programming, so there's very
-// minimal starter code here for you.
-
-// All of your programming assignments' driver files (i.e., the .cpp file
-// containing the main() function) should have a program header at the
-// top. Fill in the program header below:
-
 /*
  * Author: <Jenna Rivera>
  * Program description: <Simulates a basket ball game between 2 players>
  */
 
-// Recall: this is the syntax for importing / including system-installed
-// header files in C++. The <iostream> header file gives you access to
-// input / output (I/O) tools, like std::cout, std::cin, and std::endl, so
-// that your program can interface with the terminal
-#include <iostream>
 
-// You'll also need <cstdlib> and <ctime> to use srand() and rand() for
-// pseudorandom number generation (PRNG)
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-// TODO: Create many other functions here (you'll want several of them to
-// satisfy the course's style guidelines: 
-// https://web.engr.oregonstate.edu/~songyip/Teaching/CS16X/cpp_style_guideline.pdf)
 
 
 /*********************************************************************
@@ -242,18 +224,10 @@ int play_again (int play) {
 
 
 
-// Recall: This is the main() function. It's the entrypoint to your program
-// (i.e., it is where your program will both start and end).
-int main() {
-	// This line seeds your PRNG. You don't need to call srand() anywhere else.
-	// After it's been called, you can then proceed to use the rand() function
-	// to generate pseudorandom numbers at any point in your program.
-	srand(time(nullptr));
 
-	// TODO: complete the main function, having it call other functions (and
-	// perhaps having those functions call other functions, and so on) to
-	// complete the program as described in the assignment description document
-	// on Canvas.;
+int main() {
+
+	srand(time(nullptr));
 
 int play;
 /* in a do while loop so user has a choice to play again*/
